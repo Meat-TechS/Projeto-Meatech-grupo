4af1f192c8dd87b89106a3169164b2325bb3955d
Sabrina-Toledo <sabrina.toledo@sptech.school>
2026-04-09 20:35:08 -0300

    código do arduino

---
+++ Código-Arduino/codigo_arduino.ino
@@ -0,0 +1,29 @@
+#include <Ultrasonic.h>
+
+int pinoTrigger = 12;
+int pinoEcho = 13;
+HC_SR04 sensor(pinoTrigger, pinoEcho);
+const int PINO_SENSOR_TEMPERATURA = A0;
+float temperaturaCelsius;
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+  float distancia = sensor.distance();
+  int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
+  temperaturaCelsius = (valorLeitura * 5.0 / 1023.0) / 0.01;
+
+
+
+  if (distancia > 0) {
+    Serial.println(1);
+  } else {
+    Serial.println(0);
+  }
+
+  Serial.println(temperaturaCelsius);
+
+  delay(1000);
+}
