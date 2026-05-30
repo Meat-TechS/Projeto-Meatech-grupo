9bd527f9777eb3f5bd1e08013d2ca33145733902
josilva129 <joao.silva129@sptech.school>
2026-05-30 19:32:47 -0300

    logica para recuperar os dados para utilizar na kpi com porta em tempo ideal

--- web-data-viz-main/src/models/dashboardModel.js
+++ web-data-viz-main/src/models/dashboardModel.js
@@ -73,12 +73,29 @@ function totalCamaras() {
 }
 
 function portaIdeal() {
-    const instrucaoSql = `SELECT COUNT(*) AS portasIdeais
-FROM registro r
-JOIN sensor s 
+    const instrucaoSql =
+        `
+   SELECT COUNT(DISTINCT c.idCamara) AS portasIdeais
+FROM camarafria c
+JOIN sensor s
+    ON s.fkCamara = c.idCamara
+JOIN registro r
     ON r.fkSensor = s.idSensor
 WHERE s.tipoSensor = 'porta'
-AND r.registroPorta = 0;`;
+  AND r.idRegistro IN (
+      SELECT MAX(idRegistro)
+      FROM registro
+      GROUP BY fkSensor
+  )
+  AND (
+      r.registroPorta = 0
+      OR (
+          r.registroPorta = 1
+          AND TIMESTAMPDIFF(MINUTE, r.dtHora, NOW()) <= 10
+      )
+  );
+ 
+`;
 
     return database.executar(instrucaoSql);
 }
