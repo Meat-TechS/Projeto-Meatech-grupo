8dac531c9d66a95010bdc61ea59aa9b882e61cbd
thay-bs <thais.carneiro@sptech.school>
2026-06-02 20:51:57 -0300

    arrumando js das camaras

--- web-data-viz-main/public/dashboard/js/camara.js
+++ web-data-viz-main/public/dashboard/js/camara.js
@@ -1,61 +1,20 @@
-function totalCamarasFrias() {
-
-    fetch("totalCamaras") // chama a rota no backend
-        .then(res => res.json()) // converte resposta para JSON
-        .then(data => { // recebe o resultado do backend
-
-            console.log("Total camaras", data) // debug da resposta
-
-            // backend retorna array, então acessamos o primeiro objeto
-            document.getElementById("totalCamaras").innerHTML =
-                data[0].total_camaras;
-
-        })
-        .catch(erro => {
-            console.log("Erro ao buscar KPI", erro) // erro da API
-        })
-}
-
-function carregarCamarasCriticas() {
-    fetch("contarCamarasCriticas")
-        .then(res => res.json())
-        .then(data => {
-
-            document.getElementById("alerta").innerHTML = data[0].camaras_criticas;
-        })
-        .catch(erro => {
-            console.log("Erro ao buscar KPI", erro)
-        })
-}
-
-function camarasFaixaIdeal() {
-    fetch("camarasIdeal")
-        .then(res => res.json())
-        .then(data => {
-            console.log("KPI Faixa Ideal", data);
-
-            document.getElementById("seguro").innerHTML = data[0].camaras_faixa_ideal;
-        })
-        .catch(erro => {
-            console.log("Erro ao buscar KPI", erro)
-        })
-}
-
 function listarCamaras() {
     fetch('/camaras/listar')
         .then(function (resposta) {
             return resposta.json();
         }).then(function (camaras) {
+
             let container = document.getElementById('kpisContainer');
+
             container.innerHTML = ''
 
             let totalAlerta = 0;
             let totalSeguro = 0;
             for (let i = 0; i < camaras.length; i++) {
                 let camara = camaras[i];
-                let status = "Seguro";
 
                 if (camara.temperatura < 0 || camara.temperatura > 4) {
+                    totalAlerta++
                     container.innerHTML += `
                 <div class="kpi-camara">
                     <div class="kpi-header">
@@ -76,6 +35,7 @@ function listarCamaras() {
                 </div>
             </div>`;
                 } else {
+                    totalSeguro++
                     container.innerHTML += `
                 <div class="kpi-camara">
                     <div class="kpi-header">
@@ -104,9 +64,5 @@ function listarCamaras() {
             console.log('erro ao listar camaras');
         })
 }
-
-// totalCamarasFrias()
-// carregarCamarasCriticas()
-// camarasFaixaIdeal()
 
 listarCamaras()
