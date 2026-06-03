53ffa111dbe70ee3932b2bbf66f6bb53de1fe633
thay-bs <thais.carneiro@sptech.school>
2026-06-03 18:26:41 -0300

    adicionando metrica de alerta nas camaras

--- web-data-viz-main/public/dashboard/js/camara.js
+++ web-data-viz-main/public/dashboard/js/camara.js
@@ -10,11 +10,13 @@ function listarCamaras() {
 
             let totalAlerta = 0;
             let totalSeguro = 0;
+            let totalCritico = 0;
+
             for (let i = 0; i < camaras.length; i++) {
                 let camara = camaras[i];
 
-                if (camara.temperatura < 0 || camara.temperatura > 4) {
-                    totalAlerta++
+                if (camara.temperatura < 0 || camara.temperatura > 7) {
+                    totalCritico++
                     container.innerHTML += `
                 <div class="kpi-camara">
                     <div class="kpi-header">
@@ -26,7 +28,7 @@ function listarCamaras() {
                 <h2>${camara.temperatura}º C</h2>
 
                 <div class="status-div">
-                <p class="status">Alerta</p>
+                <p class="status">Critico</p>
                 </div>
 
             <a href="./infocamaras.html?id=${camara.idCamara}">
@@ -34,7 +36,29 @@ function listarCamaras() {
             </a>
                 </div>
             </div>`;
-                } else {
+        } else if (camara.temperatura > 4 && camara.temperatura <= 7){
+                    totalAlerta++
+                    container.innerHTML += `
+                <div class="kpi-camara">
+                    <div class="kpi-header">
+                 <h5>${camara.identificacao}</h5>
+                <img src="./image/cameras.png">
+                 </div>
+
+                <div class="kpi-main">
+                <h2>${camara.temperatura}º C</h2>
+
+                <div class="status-divA">
+                <p class="statusalert">Alerta</p>
+                </div>
+
+            <a href="./infocamaras.html?id=${camara.idCamara}">
+                Informações da câmara
+            </a>
+                </div>
+            </div>`;
+
+            } else {
                     totalSeguro++
                     container.innerHTML += `
                 <div class="kpi-camara">
@@ -59,10 +83,11 @@ function listarCamaras() {
             document.getElementById('totalCamaras').innerHTML = camaras.length
             document.getElementById('alerta').innerHTML = totalAlerta;
             document.getElementById('seguro').innerHTML = totalSeguro;
+            document.getElementById('critico').innerHTML = totalCritico;
         })
         .catch(function (erro) {
-            console.log('erro ao listar camaras');
-        })
+    console.error("ERRO COMPLETO:", erro);
+});
 }
 
 listarCamaras()
